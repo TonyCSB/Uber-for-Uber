f1643ed3149617a96ca750c526eab634ccb523c1
TonyCSB <13817576706@163.com>
2019-02-17 04:49:38 -0500

    Update search-routes.js

--- backend/routes/search-routes.js
+++ backend/routes/search-routes.js
@@ -1,12 +1,27 @@
 const routes = require("express").Router(); 
 
 const objectModel = require('../models/object-model.js');
+const userModel = require('../models/user-model.js')
+const locationModel = require('../models/location-model.js')
 
-routes.post('/Item:Name', (req, res) => {
+routes.post('/item/:Name', (req, res) => {
 
     var regex = new RegExp(req.params.Name, 'i');
     return objectModel.find({Name: regex}, function(err,q){
         return res.send(q);
+    });
+});
+
+routes.get('/location/:Name', (req, res) => {
+
+    var regex = new RegExp(req.params.Name, 'i');
+
+    return locationModel.find({Building: regex}, function(err,r){
+        return userModel.find({Location: r._id}, function(err,q){
+            return objectModel.find({Owner: q._id}, function(err,p){
+                return res.send(p);
+            });
+        });
     });
 });
 
