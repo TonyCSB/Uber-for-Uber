b419a155e4546a0d445c40cedcfa2b662dfa5923
TonyCSB <13817576706@163.com>
2019-02-17 03:06:04 -0500

    Search Update

--- backend/routes/search-routes.js
+++ backend/routes/search-routes.js
@@ -2,17 +2,14 @@ const routes = require("express").Router();
 
 const objectModel = require('../models/object-model.js');
 
-routes.get('/obj:Name', (req, res) => {
-
-    // req.body is the data passed in
-    // res.send({ <DATA HERE >})
+routes.post('/Item:Name', (req, res) => {
 
     var regex = new RegExp(req.params.Name, 'i');
     return objectModel.find({Name: regex}, function(err,q){
         return res.send(q);
     });
-
 });
+
 
 
 module.exports = routes;
